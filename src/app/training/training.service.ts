0f0a65c3f1e4d7ccf1f1046dff600530cd1b8308
mhazak <hazak.marek@gmail.com>
2022-06-27 20:20:48 +0200

    [cleaning] removed hardcoded code

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -9,12 +9,7 @@ import { Injectable } from "@angular/core";
 export class TrainingService {
 	constructor (private db: AngularFirestore) {}
 
-	private availableExercises: Exercise[] = [
-		{ id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
-		{ id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
-		{ id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
-		{ id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
-	];
+	private availableExercises: Exercise[] = [];
 
 	exerciseStarted = new Subject<Exercise | null>();
 	exerciseChanged = new Subject<Exercise[]>();
