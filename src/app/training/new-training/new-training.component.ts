3d359ede4139adf93b1328531419025dbbbc1566
mhazak <hazak.marek@gmail.com>
2022-06-27 19:59:33 +0200

    [dev] fetching exercises from firebase

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Output, EventEmitter, ViewChild } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
 
@@ -12,12 +15,25 @@ export class NewTrainingComponent implements OnInit {
 
 	@ViewChild('selectedExercise') selectedExercise!: any;
 	@Output() trainingStarts = new EventEmitter<void>();
-	exercises!: Exercise[];
+	exercises!: Observable<Exercise[]>;
 
-	constructor(private trainingService: TrainingService) { }
+	constructor(private trainingService: TrainingService, private db: AngularFirestore) { }
 
   	ngOnInit(): void {
-		this.exercises = this.trainingService.getExercises();
+		this.exercises = this.db
+		.collection('availableExercises')
+		.snapshotChanges()
+		.pipe(
+			map(docData => {
+				return docData.map(doc => {
+					const data: any = doc.payload.doc.data()
+					return {
+						id: doc.payload.doc.id,
+						...data
+					}
+				})
+			})
+		)
   	}
 
 	startTraining (form: NgForm) {
