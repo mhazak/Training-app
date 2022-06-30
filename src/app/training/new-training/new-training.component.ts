37d0af5178e86946a704e7167f83f7bbcc188108
mhazak <hazak.marek@gmail.com>
2022-06-30 18:44:11 +0200

    [dev] ngrx observables

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter, ViewChild, OnDestroy } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild } from '@angular/core';
 
 import { NgForm } from '@angular/forms';
 import { Store } from '@ngrx/store';
@@ -6,6 +6,7 @@ import { Observable, Subscription } from 'rxjs';
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
 
+import * as fromTraining from '../training.reducer';
 import * as fromRoot from '../../app.reducer';
 
 @Component({
@@ -14,33 +15,26 @@ import * as fromRoot from '../../app.reducer';
 	styleUrls: ['./new-training.component.css']
 })
 
-export class NewTrainingComponent implements OnInit, OnDestroy {
+export class NewTrainingComponent implements OnInit {
 
 	@ViewChild('selectedExercise') selectedExercise!: any;
 	@Output() trainingStarts = new EventEmitter<void>();
-	exercises!: Exercise[];
+	exercises$: Observable<Exercise[]>;
 	subscription: Subscription;
 	isLoading$: Observable<boolean>;
 	loadingSubscription: Subscription;
 
-	constructor(private trainingService: TrainingService, private store: Store<fromRoot.State>) { }
+	constructor(private trainingService: TrainingService, private store: Store<fromTraining.State>) { }
 
 	ngOnInit(): void {
 
 		this.isLoading$ = this.store.select(fromRoot.getIsLoading);
-		this.subscription = this.trainingService.exerciseChanged.subscribe((result: Exercise[]) => {
-			this.exercises = result;
-		});
+		this.exercises$ = this.store.select(fromTraining.getAvailableExercises);
 		this.fetchExercises();
   	}
 
 	fetchExercises() {
 		this.trainingService.fetchExercises();
-	}
-
-	ngOnDestroy () {
-		if (this.subscription)
-			this.subscription.unsubscribe();
 	}
 
 	startTraining (form: NgForm) {
