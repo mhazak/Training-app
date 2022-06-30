b7a6affb35134a6292dfe14c9de7f4c10d473b30
mhazak <hazak.marek@gmail.com>
2022-06-30 18:59:31 +0200

    [dev] ngrx approach

--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -1,29 +1,30 @@
-import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
+
+import * as fromTraining from '../training.reducer';
 
 @Component({
   selector: 'app-past-trainings',
   templateUrl: './past-trainings.component.html',
   styleUrls: ['./past-trainings.component.css']
 })
-export class PastTrainingsComponent implements OnInit, AfterViewInit, OnDestroy {
+export class PastTrainingsComponent implements OnInit, AfterViewInit {
 
 	displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
 	datasource = new MatTableDataSource<Exercise>();
-	subscription: Subscription;
 
 	@ViewChild(MatSort) sort!: MatSort;
 	@ViewChild(MatPaginator) paginator!: MatPaginator;
 
-	constructor(private trainingService: TrainingService) { }
+	constructor(private trainingService: TrainingService, private store: Store<fromTraining.State>) { }
 
 	ngOnInit(): void {
-		this.subscription = this.trainingService.completedExercisesChanged.subscribe((result: Exercise[]) => {
+		this.store.select(fromTraining.getFinishedExercises).subscribe((result: Exercise[]) => {
 			this.datasource.data = result;
 		})
 		this.trainingService.getExerciseHistory();
@@ -32,11 +33,6 @@ export class PastTrainingsComponent implements OnInit, AfterViewInit, OnDestroy 
 	ngAfterViewInit() {
 		this.datasource.sort = this.sort;
 		this.datasource.paginator = this.paginator;
-	}
-
-	ngOnDestroy() {
-		if (this.subscription)
-			this.subscription.unsubscribe();
 	}
 
 	doFilter(event: KeyboardEvent) {
