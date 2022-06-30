a66ec025546af0f6fcdf9b707a2225976161ce22
mhazak <hazak.marek@gmail.com>
2022-06-30 18:55:47 +0200

    [dev] ngrx approach

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -3,16 +3,21 @@ import { MatDialog } from '@angular/material/dialog';
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
 
-import { Subscription } from 'rxjs';
+import { Subscription, take } from 'rxjs';
 
 import { StopTrainingComponent } from './stop-training/stop-training.component';
+import { Store } from '@ngrx/store';
+import * as fromTraining from '../training.reducer';
+
 @Component({
   selector: 'app-current-training',
   templateUrl: './current-training.component.html',
   styleUrls: ['./current-training.component.css']
 })
+
 export class CurrentTrainingComponent implements OnInit {
-	constructor(private dialog: MatDialog, private trainingService: TrainingService) { }
+
+	constructor(private dialog: MatDialog, private trainingService: TrainingService, private store: Store<fromTraining.State>) { }
 
 	interval: any;
 	progress = 0;
@@ -26,24 +31,24 @@ export class CurrentTrainingComponent implements OnInit {
 	}
 
 	startOrContinue() {
-		if (!this.trainingService.getRunningExercise()?.duration) {
-			return;
-		}
-		const runningExercise = this.trainingService.getRunningExercise();
 
-		if (runningExercise?.duration) {
-			// duration of training devided by 100 (percent) multiple by 1000 (ms)
-			const step = runningExercise?.duration / 100 * 1000;
-			this.interval = setInterval(() => {
-				this.progress += 1;
-				if (this.progress >= 100) {
-					this.trainingService.completedExercise();
-					this.status = 'Your workout is done. Well done!';
-					clearInterval(this.interval);
-					return;
-				}
-			}, step);
-		}
+		this.store.select(fromTraining.getActiveTraining).pipe(take(1)).subscribe(training => {
+
+			if (training) {
+				// duration of training devided by 100 (percent) multiple by 1000 (ms)
+				const step = training?.duration / 100 * 1000;
+				this.interval = setInterval(() => {
+					this.progress += 1;
+					if (this.progress >= 100) {
+						this.trainingService.completedExercise();
+						this.status = 'Your workout is done. Well done!';
+						clearInterval(this.interval);
+						return;
+					}
+				}, step);
+			}
+		})
+
 	}
 
 	stopTraining () {
